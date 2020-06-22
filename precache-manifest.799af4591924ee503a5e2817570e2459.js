6dbc4dc6fe58f9e0c703546a937b6cbe5f6d6564
UIDeveloper569 <uideveloper569@gmail.com>
2020-06-22 18:14:05 +0530

    Updates

---
+++ precache-manifest.799af4591924ee503a5e2817570e2459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99593b3d6e6e5512f337d99d4cb2ea9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d801058534719ceadd5b",
+    "url": "/monsters-rolodex/static/css/main.560859a3.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d801058534719ceadd5b",
+    "url": "/monsters-rolodex/static/js/main.6e99165a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
